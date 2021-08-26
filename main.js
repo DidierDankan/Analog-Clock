f409c6f9ac49bbac9b5b9109f732d8a9b24a0ad1
Didier Dankan <didi_archer@hotmail.com>
2021-08-26 19:06:55 +0200

    Analog Clock finish

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log('js ok')
+setInterval(setClock, 1000)
+
+const hoursHand = document.getElementById('hour');
+const minutesHand = document.getElementById('minutes');
+const secondsHand = document.getElementById('seconds');
+
+function setClock() {
+    const currentDate = new Date()
+    const secondsRatio = currentDate.getSeconds() / 60;
+    const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
+    const hoursRatio = (minutesRatio + currentDate.getHours()) / 12; 
+    setRotation(secondsHand, secondsRatio)
+    setRotation(minutesHand, minutesRatio)
+    setRotation(hoursHand, hoursRatio)
+}
+
+function setRotation(element, rotationRatio) {
+    element.style.setProperty('--rotation', rotationRatio * 360)
+}
+
+setClock()
